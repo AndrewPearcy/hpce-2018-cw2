5fbdbd40a20da86fead0118098b61d7418ecccc3
Andrew Pearcy <abp14@ic.ac.uk>
2019-02-04 14:52:34 +0000

    regenerating data for second graph

--- results/direct_outer_time_versus_p.sh
+++ results/direct_outer_time_versus_p.sh
@@ -6,6 +6,6 @@ PS="1 2 3 4"
 # Iterate over them and print (echo) them
 for P in $PS; do
     # Run the program with the chosen K, and save to dump_K.csv
-    bin/time_fourier_transform hpce.abp14.direct_fourier_transform_parfor_inner ${P} 30 "${P}, " > dump_${P}.csv
+    bin/time_fourier_transform hpce.abp14.direct_fourier_transform_parfor_outer ${P} 30 "${P}, " > dump_${P}.csv
 done
 
